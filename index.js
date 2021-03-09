303e0175ad5a1408fd2885f3489ab2827f4e005c
taylorsieling <taylorasieling@gmail.com>
2021-03-09 12:14:22 -0600

    commit tree 64fae6bf0319be7ee83c62a36c92a195bfd412df

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 25
+let num4 = 7
+let mod = num3 % num4
+
+let max = Math.max(20, 5, 15, 10)
